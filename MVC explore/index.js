b25a3eb625b8b68a92b6f109a3f848c19e59e5f0
sujonphero <sujonsheikh.in@gmail.com>
2023-08-15 15:50:12 +0600

    done mvc

--- MVC explore/index.js
+++ MVC explore/index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const usersRouter = require('./routes/users.route')
+
+const PORT = 3000;
+app.use(usersRouter)
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use((req, res, next) => {
+    res.status(404).json({ message: "Opps recource not found." })
+})
+
+app.listen(PORT, (req, res) => {
+    console.log('Server is running on', PORT)
+});
---
+++ MVC explore/models/users.model.js
@@ -0,0 +1,6 @@
+const users = [
+    { name: "sujon", age: "23" },
+    { name: "sheikh", age: "24" }
+];
+
+module.exports = users;
