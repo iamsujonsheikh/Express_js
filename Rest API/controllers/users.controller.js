95354a194a5de30e34d1a5e05153e535b7491c12
sujonphero <sujonsheikh.in@gmail.com>
2023-08-16 12:46:20 +0600

    done my rest_api

--- Rest API/controllers/users.controller.js
+++ Rest API/controllers/users.controller.js
@@ -7,7 +7,6 @@ getAllUser = (req, res) => {
     res.status(200).json({ users })
 };
 
-
 //Create users
 createUser = (req, res) => {
     const newUser = {
@@ -16,6 +15,24 @@ createUser = (req, res) => {
         email: req.body.email,
     }
     users.push(newUser)
-    res.status(200).json(users)
+    res.status(201).json(users)
 }
-module.exports = { getAllUser, createUser };
+
+//Update user
+updateUser = (req, res) => {
+    const userId = req.params.id;
+    const { userName, email } = req.body;
+    users.filter((user) => user.id === userId).map((selectedUser) => {
+        selectedUser.userName = userName;
+        selectedUser.email = email;
+    })
+    res.status(200).json({ message: users });
+}
+
+//Delete user
+deleteUser = (req, res) => {
+    const userId = req.params.id;
+    const allNewUser = users.filter((user) => user.id !== userId);
+    res.status(200).json({ message: allNewUser });
+}
+module.exports = { getAllUser, createUser, updateUser, deleteUser };
--- Rest API/routes/users.route.js
+++ Rest API/routes/users.route.js
@@ -1,9 +1,11 @@
-const { getAllUser, createUser } = require('../controllers/users.controller');
+const { getAllUser, createUser, updateUser, deleteUser } = require('../controllers/users.controller');
 
 const router = require('express').Router();
 
 // Users route
 router.get('/', getAllUser);
 router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 module.exports = router;
